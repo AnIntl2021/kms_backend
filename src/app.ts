524a3e5b5d8c28fe8122308bab959af7a06b982f
Affan Kaskar <affanabbas89@gmail.com>
2026-06-07 19:04:15 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -186,6 +186,7 @@ app.use(cors({
 }));
 
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
+app.use('/api/uploads', express.static(path.join(process.cwd(), 'uploads')));
 app.use(morgan(config.env === 'development' ? 'dev' : 'combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
